6a23d29d5dde8e08cb67c3684256046839710091
Ario <dev@ariochain.info>
2018-12-05 18:12:16 +0100

    checker

---
+++ README.md
@@ -0,0 +1,7 @@
+# mn-checker
+
+1. download file to your arionum node directory
+2. set up your array of ip addresses to check
+3. set up your email address for notifications
+4. set up cron task to check status periodicaly
+
---
+++ checkmn.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once("include/init.inc.php");
+$block=new Block;
+$current=$block->current();
+
+$hostname = 'https://www.ariochain.info';   // report hostname
+$reportUri = '/api/v1/public/peers.json';   // report uri
+$notifyEmail = 'dev@ariochain.info';        // send mail to
+$show = true;                               // show results in terminal
+
+
+// your ip addresses you want to check
+
+$checknodes = array(
+    "5.5.95.239",
+);
+
+
+// load report
+
+$apiUrl = $hostname.$reportUri;
+$ariochainreport = file_get_contents($apiUrl);
+$json=json_decode($ariochainreport,true); // formated result
+
+// our nodes
+foreach ($checknodes as $node) {
+   
+    if ($show) echo $node." ";
+    
+    // get node's current block
+    $url = "http://".$node."/peer.php?q=";
+    $data = peer_post($url."currentBlock", [], 5);
+
+    // report data
+    foreach ($json['peers'] as $peer) {
+
+        if ($peer['ip'] == $node) { // our node matching report entry
+
+            if ($data['height'] !== $peer['lastblock']) { // if reporting has wrong number
+                $peer['lastblock'] == $data['height']; // display current block
+            } else
+            if ($peer['status'] == 'behind') { // if reporting has last block, check if status is behind
+
+                // send notification
+                $to = $notifyEmail;
+                $subject = "Node ".$node." behind";
+                $txt = "Your node ".$node." is behind.";
+                $headers = "From: ".$notifyEmail;
+
+                mail($to,$subject,$txt,$headers);
+            }
+
+            if ($show) echo $peer['lastblock']." ";
+            if ($show) echo $peer['status']." ";
+
+        }
+    }
+
+    if ($show) echo "\n";
+}
+
+?>
