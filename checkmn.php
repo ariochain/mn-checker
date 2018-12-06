774505bc58652d00ca858da7d17a2c14726235a8
pxgamer <owzie123@gmail.com>
2018-12-06 16:21:00 +0000

    Update comment casing

--- checkmn.php
+++ checkmn.php
@@ -2,13 +2,13 @@
 
 require_once __DIR__.'/include/init.inc.php';
 
-$hostname = 'https://www.ariochain.info';   // report hostname
-$reportUri = '/api/v1/public/peers.json';   // report uri
-$notifyEmail = 'dev@ariochain.info';        // send mail to
-$show = true;                               // show results in terminal
+$hostname = 'https://www.ariochain.info';   // Report hostname
+$reportUri = '/api/v1/public/peers.json';   // Report URI
+$notifyEmail = 'dev@ariochain.info';        // Send mail to
+$show = true;                               // Show results in terminal
 
 
-// Your ip addresses you want to check
+// Your IP addresses you want to check
 
 $checkNodes = [
     '5.5.95.239',
@@ -19,7 +19,7 @@ $checkNodes = [
 
 $apiUrl = $hostname.$reportUri;
 $ariochainReport = file_get_contents($apiUrl);
-$json = json_decode($ariochainReport, true); // formated result
+$json = json_decode($ariochainReport, true); // Formated result
 
 // Our nodes
 foreach ($checkNodes as $node) {
@@ -27,17 +27,18 @@ foreach ($checkNodes as $node) {
         echo $node.' ';
     }
 
-    // get node's current block
+    // Get node's current block
     $url = 'http://'.$node.'/peer.php?q=';
     $data = peer_post($url.'currentBlock', [], 5);
 
-    // report data
+    // Report data
     foreach ($json['peers'] as $peer) {
-        if ($peer['ip'] == $node) { // our node matching report entry
-            if ($data['height'] !== $peer['lastblock']) { // if reporting has wrong number
-                $peer['lastblock'] == $data['height']; // display current block
-            } elseif ($peer['status'] == 'behind') { // if reporting has last block, check if status is behind
-                // send notification
+        // Our node matching report entry
+        if ($peer['ip'] == $node) {
+            if ($data['height'] !== $peer['lastblock']) { // If reporting has wrong number
+                $peer['lastblock'] == $data['height']; // Display current block
+            } elseif ($peer['status'] == 'behind') { // If reporting has last block, check if status is behind
+                // Send notification
                 $to = $notifyEmail;
                 $subject = 'Node '.$node.' behind';
                 $txt = 'Your node '.$node.' is behind.';
