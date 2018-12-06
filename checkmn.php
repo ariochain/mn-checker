2730e5e0fd77098b546b27674eba3fca9fa8ecff
pxgamer <owzie123@gmail.com>
2018-12-06 16:18:43 +0000

    Update to follow PSR-2

--- checkmn.php
+++ checkmn.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once("include/init.inc.php");
+require_once __DIR__.'/include/init.inc.php';
 
 $hostname = 'https://www.ariochain.info';   // report hostname
 $reportUri = '/api/v1/public/peers.json';   // report uri
@@ -8,54 +8,54 @@ $notifyEmail = 'dev@ariochain.info';        // send mail to
 $show = true;                               // show results in terminal
 
 
-// your ip addresses you want to check
+// Your ip addresses you want to check
 
-$checknodes = array(
-    "5.5.95.239",
-);
+$checkNodes = [
+    '5.5.95.239',
+];
 
 
-// load report
+// Load report
 
 $apiUrl = $hostname.$reportUri;
-$ariochainreport = file_get_contents($apiUrl);
-$json=json_decode($ariochainreport,true); // formated result
+$ariochainReport = file_get_contents($apiUrl);
+$json = json_decode($ariochainReport, true); // formated result
 
-// our nodes
-foreach ($checknodes as $node) {
-   
-    if ($show) echo $node." ";
-    
+// Our nodes
+foreach ($checkNodes as $node) {
+    if ($show) {
+        echo $node.' ';
+    }
+
     // get node's current block
-    $url = "http://".$node."/peer.php?q=";
-    $data = peer_post($url."currentBlock", [], 5);
+    $url = 'http://'.$node.'/peer.php?q=';
+    $data = peer_post($url.'currentBlock', [], 5);
 
     // report data
     foreach ($json['peers'] as $peer) {
-
         if ($peer['ip'] == $node) { // our node matching report entry
-
             if ($data['height'] !== $peer['lastblock']) { // if reporting has wrong number
                 $peer['lastblock'] == $data['height']; // display current block
-            } else
-            if ($peer['status'] == 'behind') { // if reporting has last block, check if status is behind
-
+            } elseif ($peer['status'] == 'behind') { // if reporting has last block, check if status is behind
                 // send notification
                 $to = $notifyEmail;
-                $subject = "Node ".$node." behind";
-                $txt = "Your node ".$node." is behind.";
-                $headers = "From: ".$notifyEmail;
+                $subject = 'Node '.$node.' behind';
+                $txt = 'Your node '.$node.' is behind.';
+                $headers = 'From: '.$notifyEmail;
 
-                mail($to,$subject,$txt,$headers);
+                mail($to, $subject, $txt, $headers);
             }
 
-            if ($show) echo $peer['lastblock']." ";
-            if ($show) echo $peer['status']." ";
-
+            if ($show) {
+                echo $peer['lastblock'].' ';
+            }
+            if ($show) {
+                echo $peer['status'].' ';
+            }
         }
     }
 
-    if ($show) echo "\n";
+    if ($show) {
+        echo "\n";
+    }
 }
-
-?>
